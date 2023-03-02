e7e0e047f1d4c4c0e0d80442dd156b83c47271dc
宋旻家 <100329489+MinChia0310@users.noreply.github.com>
2023-03-02 13:19:55 +0800

    Delete A1083366_0224 directory

--- A1083366_0224/A1083366_0224_2.java
+++
@@ -1,8 +0,0 @@
-public class A1083366_0224_2{
-    public static void main(String[] args){
-        java.util.Scanner sc=new java.util.Scanner(System.in);
-        System.out.print("請輸入攝氏溫度:");
-        float input=sc.nextFloat();
-        System.out.println("華氏溫度為:"+(input*9/5+32));
-    }
-}
